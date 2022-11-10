892a8a3cef3ca21f2bd4dec346b4e23502671503
Mutabor <sinner885@gmail.com>
2022-11-10 21:42:24 +0200

    исправление названия lists

---
+++ lists/tests.py
@@ -0,0 +1,10 @@
+from django.urls import resolve
+from django.test import TestCase
+from lists.views import home_page
+class HomePageTest(TestCase):
+    '''тест домашней страницы'''
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест: корневой url преобразуется в представление
+        домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
